eb41fbc27cdb64b34809c9e871fbc25a624a887a
Parth <parthvpujare@gmail.com>
2025-11-09 09:11:32 +0530

    Added time complexities

--- trie.py
+++ trie.py
@@ -9,7 +9,7 @@ class Trie:
     def __init__(self):
         self.root = Node()
 
-    def insert (self, word):
+    def insert (self, word):                                    #O(m) m is the length of the word
         current_node = self.root
 
         for c in word:
@@ -20,7 +20,7 @@ class Trie:
         
         current_node.is_end_of_word = True
 
-    def search(self,word):
+    def search(self,word):                                    #O(m) m is the length of the word
         current_node = self.root
 
         for c in word:
@@ -31,10 +31,10 @@ class Trie:
         return current_node.is_end_of_word
         
     
-    def delete(self,word):
+    def delete(self,word):                                    #O(m) m is the length of the word
         self._delete(self.root, word, 0)
 
-    def has_prefix(self, prefix):
+    def has_prefix(self, prefix):                              #O(m) m is the length of the word
         current_node = self.root
 
         for c in prefix:
@@ -44,7 +44,7 @@ class Trie:
 
         return True
 
-    def starts_with(self, prefix):
+    def starts_with(self, prefix):                                    #O(m+k) m is the length of the prefix and k is the total number of characters in all sufixes
         words = []
         current_node = self.root
 
@@ -64,7 +64,7 @@ class Trie:
         _dfs(current_node, list(prefix))
         return words
 
-    def list_words(self):
+    def list_words(self):                                    #O(n) n is the number of words in the Trieß
         words = []
         def _dfs(current_node, path):
             if current_node.is_end_of_word:
